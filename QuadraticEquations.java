e18aa27d002944d4dcc5fa81795f7c5c8c86796f
Muxtarli <75085470+Muxtarli@users.noreply.github.com>
2020-12-15 23:23:50 +0400

    Add files via upload

---
+++ QuadraticEquations.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class QuadraticEquations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first num: ");
+        int a = sc.nextInt();
+        System.out.println("Enter second num: ");
+        int b = sc.nextInt();
+        System.out.println("Enter third num: ");
+        int c = sc.nextInt();
+
+        if (b * b - 4 * a * c > 0)
+            System.out.println("Quadratic equation has 2 roots x1 = -2 | x2 = -3);
+        else if (b * b - 4 * a * c == 0)
+            System.out.println("Quadratic equation has 1 root");
+        else
+            System.out.println("Quadratic equation has no solution");
+
+    }
+}
